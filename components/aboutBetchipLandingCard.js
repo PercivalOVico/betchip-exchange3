4d657587beb3df16c64c2f0634f3fbab940b0d2d
Percival O Vico <percifico@gmail.com>
2022-07-21 18:17:39 +0300

    version: 0.0.4

--- components/aboutBetchipLandingCard.js
+++ components/aboutBetchipLandingCard.js
@@ -4,15 +4,15 @@ function AboutBetchipLandingCard() {
   return (
     <div>
             <div class="flex justify-center">
-  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-black shadow-lg">
     <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(7).webp" alt="" />
     <div class="p-6 flex flex-col justify-start">
       <h5 class="text-gray-900 text-xl font-medium mb-2">BETCHIP CRYPTOS</h5>
-      <p class="text-gray-700 text-base mb-4">
+      <p class="text-gray-300 text-base mb-4">
         There are thousands of crypto currency assest on the blockchain. This makes it hard for Potential and Intrested
         Investors to decide on what crypto asset to invest in,
       </p>
-      <p>
+      <p class="text-gray-300">
       Betchip Exchange helps you the Investor get the legit and porofitable crypto assest
       hence saving you alot of time on research.
       </p>
@@ -21,15 +21,15 @@ function AboutBetchipLandingCard() {
     </div>
   </div>
 
-  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-black shadow-lg">
     <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(4).webp" alt="" />
     <div class="p-6 flex flex-col justify-start">
       <h5 class="text-gray-900 text-xl font-medium mb-2">CRYPTO TRADING</h5>
-      <p class="text-gray-700 text-base mb-4">
+      <p class="text-gray-300 text-base mb-4">
        Apart From increasing on your crypto portifoilio BETCHIP EXCHANGE also offers crypto
        Trading services.
       </p>
-      <p>
+      <p  class="text-gray-300 text-base mb-4">
       BETCHIP has an eco-system that allows you to invest and benefit from monthlt, quaterly or annual 
       returns from your Investments.
       </p>
@@ -38,14 +38,14 @@ function AboutBetchipLandingCard() {
     </div>
   </div>
   
-  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+  <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-black shadow-lg">
     <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(3).webp" alt="" />
     <div class="p-6 flex flex-col justify-start">
-      <h5 class="text-gray-900 text-xl font-medium mb-2">P2P</h5>
-      <p class="text-gray-700 text-base mb-4">
+      <h5 class="text-gray-600 text-xl font-medium mb-2">P2P</h5>
+      <p class="text-gray-300 text-base mb-4">
         With the demand for crypto on the rise, Betchip Exchange is at the fore-front of this evolution
       </p>
-      <p>
+      <p class="text-gray-300 text-base mb-4">
       We make it easy for you to BUY or SELL crypto currency in exchange for Fiat or Other cryptos
       </p>
       <br></br>
